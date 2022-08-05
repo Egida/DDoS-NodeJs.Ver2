0e3c8f85063bc60a2332a4e93ab459d1455bf63e
Heaven Hevin B <96363896+le-cuong-hacker1111@users.noreply.github.com>
2022-08-06 06:29:39 +0700

    Create install.sh

---
+++ HTTPS-FORCE.js/install.sh
@@ -0,0 +1,1 @@
+sudo apt-get update; sudo apt-get upgrade -y; sudo apt-get install nodejs -y; sudo apt-get install npm -y; npm i axios && npm i fake-useragent && npm i request && npm i fs && npm i net && npm i cloudscraper && npm i colors;
